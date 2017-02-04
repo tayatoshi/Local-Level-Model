b590bf3e814a883adcd85eb2866e95988a48fc19
tayatoshi <tsk0414z@gmail.com>
2017-02-04 11:05:01 +0900

    first -commit

---
+++ Local Lebel Model.py
@@ -0,0 +1,85 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+import pandas as pd
+
+
+# In[ ]:
+
+alpha=[]
+alpha.append(np.exp(2)+80)
+for i in range(50):
+    alpha.append(alpha[i]+np.random.normal(0,50))
+y=alpha+np.random.normal(0,50,len(alpha))
+
+plt.plot(y,label="observation")
+plt.plot(alpha,label="state")
+plt.legend()
+plt.ylim(min(y)-5,max(y)+5)
+plt.show()
+
+
+# In[ ]:
+
+def kalman(a0=np.mean(alpha),p0=5000,Var_epsilon=50,Var_eta=50):
+    a = a0
+    p = p0
+    A = []
+    A_t = []
+    P_t = []
+    for i in range(50):
+        u = y[i] - a
+        f = p + Var_epsilon
+        K = p/f
+        L = 1-K
+        a_t = a + K*u
+        A_t.append(a_t)
+        p_t = p*L
+        P_t.append(p_t)
+        A.append(a)
+        a = A_t[i]
+        p = P_t[i] + Var_eta
+        
+    A_t=pd.DataFrame(A_t)
+    A_t.index=A_t.index+1
+    plt.plot(y,'-o',label='y')
+    plt.plot(alpha,label="state")
+    #plt.plot(A,label="one step")
+    plt.plot(A_t,label="filtering state")
+    #plt.plot(A_t-1.96*np.sqrt(P_t))
+    #plt.plot(A_t+1.96*np.sqrt(P_t))
+    #plt.legend()
+    plt.ylim(min(y)-50,max(y)+50)
+    plt.show()
+
+
+# In[ ]:
+
+kalman()
+
+
+# In[ ]:
+
+a=[1,2,3,4,5,6,7,8,9]
+a=pd.DataFrame(a)
+
+
+# In[ ]:
+
+a.index=a.index+1
+
+
+# In[ ]:
+
+a
+
+
+# In[ ]:
+
+
+
---
+++ julia Local Lebel Model.jl
@@ -0,0 +1,58 @@
+
+using Distributions
+using Gadfly
+
+α = Vector{Float64}(50)
+η = Normal(0,5)
+ɛ = Normal(0,5)
+α[1] = exp(2)+80.0
+for i in 2:50
+    α[i] = α[i-1] + rand(η)
+end
+y = α .+ rand(ɛ, length(α))
+
+plot(
+    layer(y=y,Geom.line,Geom.point),
+    layer(y=α,Geom.line,Theme(default_color=color("red"))),
+    Guide.manual_color_key("Legend", ["観測値", "状態"],
+                           [color("deepskyblue"), color("red")])
+    )
+
+function kalmanfilter(a0::Float64, p0::Float64, Var_ɛ::Float64, Var_η::Float64)
+    #a0:一期先状態予測(Float64)
+    #p0:一期先状態分散(Float64)
+    a = a0
+    p = p0
+    A_t = Vector{Float64}(50)
+    P_t = Vector{Float64}(50)
+    for i in 1:length(y)
+        υ = y[i] - a
+        f = p + Var_ɛ
+        K = p/f
+        L = 1 - K
+        a_t = a + K*υ
+        A_t[i] = a_t
+        p_t = p*L
+        P_t[i] = p_t
+        a = A_t[i]
+        p = P_t[i] + Var_η
+    end
+    plot(
+    layer(y=y,Geom.line),
+    layer(y=α,Geom.line,Theme(default_color=color("red"))),
+    layer(y=A_t,Geom.line,Theme(default_color=color("green"))),
+    Guide.manual_color_key("Legend", ["観測値", "真の状態","フィルタ化推定量"],
+    [color("deepskyblue"), color("red"),color("green")])
+    )
+end
+
+kalmanfilter(mean(α),5000.0,5.0,5.0)
+
+using RDatasets
+using DataFrames
+
+iris=dataset("datasets","Nile")
+
+
+
+
